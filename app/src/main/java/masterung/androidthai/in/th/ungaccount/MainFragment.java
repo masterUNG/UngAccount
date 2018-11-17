8d2649be39cf8bc4b8cee123bcdb6a1888eac1b4
masterUNG <phrombutr@gmail.com>
2018-11-17 13:16:11 +0700

    Replace Fragment Success

--- app/src/main/java/masterung/androidthai/in/th/ungaccount/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungaccount/MainFragment.java
@@ -2,10 +2,12 @@ package masterung.androidthai.in.th.ungaccount;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -18,6 +20,25 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        SignUp Controller
+        Button button = getView().findViewById(R.id.buttonSignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace fragment
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null).commit();
+            }
+        });
+
+
+    }   // Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
