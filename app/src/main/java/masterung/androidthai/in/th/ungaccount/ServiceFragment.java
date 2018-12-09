b311d92a56bfb8ea1052e343a6292ca8e23c19af
masterUNG <phrombutr@gmail.com>
2018-12-09 15:32:45 +0700

    Edit Database Firebase

--- app/src/main/java/masterung/androidthai/in/th/ungaccount/ServiceFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungaccount/ServiceFragment.java
@@ -2,6 +2,7 @@ package masterung.androidthai.in.th.ungaccount;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -11,14 +12,25 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ServiceFragment extends Fragment {
+
+    private String currentUid;
 
 
     public ServiceFragment() {
@@ -30,11 +42,81 @@ public class ServiceFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+//        Find Uid
+        findUid();
+
 //        Create Toolbar
         createToolbar();
 
+//        Post Controller
+        postController();
 
     }   // Main Method
+
+    private void findUid() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        currentUid = firebaseAuth.getUid();
+    }
+
+    private void postController() {
+        Button button = getView().findViewById(R.id.buttonPost);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText editText = getView().findViewById(R.id.edittextMessage);
+                final String messageString = editText.getText().toString().trim();
+
+                if (messageString.isEmpty()) {
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.normalDialog(getString(R.string.title_have_space), getString(R.string.message_have_space));
+                } else {
+
+                    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = firebaseDatabase.getReference().child("User").child(currentUid);
+
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                            UserModel userModel1 = new UserModel(
+                                    userModel.getAvatarString(),
+                                    messageString,
+                                    userModel.getNameString(),
+                                    userModel.getUidString());
+
+                            replaceMessage(userModel1);
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    editText.setText("");
+
+                }   // if
+
+            }
+        });
+    }
+
+    private void replaceMessage(UserModel userModel1) {
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("User").child(currentUid);
+        databaseReference.setValue(userModel1).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getActivity(), "Upload Message OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
